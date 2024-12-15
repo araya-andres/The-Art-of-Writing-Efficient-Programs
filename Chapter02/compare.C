be724eb729de7464b1b094ea0efa130cbc29c385
Andres Araya <araya.andres@gmail.com>
2024-12-15 08:53:19 -0600

    use only pointers for compare

--- Chapter02/compare.C
+++ Chapter02/compare.C
@@ -1,9 +1,7 @@
 // 03 using int instead of unsigned int in compare()
 bool compare(const char* s1, const char* s2) {
     if (s1 == s2) return false;
-    for (int i1 = 0, i2 = 0; ; ++i1, ++i2) {
-        if (s1[i1] != s2[i2]) return s1[i1] > s2[i2];
-    }
-    return false;
+    while (*s1 && *s1 == *s2) s1++, s2++;
+    return *s1 > *s2;
 }
 
